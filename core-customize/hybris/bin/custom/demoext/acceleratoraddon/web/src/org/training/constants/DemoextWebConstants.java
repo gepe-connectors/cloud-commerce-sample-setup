1bca0d9c2f395f1b686c3b8381b7833ba9aff65e
Petrov, Georgi <Georgi.Petrov@softwareag.com>
2021-03-01 14:43:32 +0200

    Add a demo custom extension.

---
+++ core-customize/hybris/bin/custom/demoext/acceleratoraddon/web/src/org/training/constants/DemoextWebConstants.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package org.training.constants;
+
+/**
+ * Global class for all Demoext web constants. You can add global constants for your extension into this class.
+ */
+public final class DemoextWebConstants
+{
+	//Dummy field to avoid pmd error - delete when you add the first real constant!
+	public static final String deleteThisDummyField = "DELETE ME";
+
+	private DemoextWebConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
---
+++ core-customize/hybris/bin/custom/demoext/src/org/training/constants/DemoextConstants.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package org.training.constants;
+
+/**
+ * Global class for all demoext constants. You can add global constants for your extension into this class.
+ */
+@SuppressWarnings({ "deprecation", "squid:CallToDeprecatedMethod" })
+public final class DemoextConstants extends GeneratedDemoextConstants
+{
+	public static final String EXTENSIONNAME = "demoext"; //NOSONAR
+
+	private DemoextConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
