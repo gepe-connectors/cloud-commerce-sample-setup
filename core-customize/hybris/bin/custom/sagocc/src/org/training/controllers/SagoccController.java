8ef87a0072b415baff5ea0f00b3d7beca36fc13e
gepe <georgi.petrov@softwareag.com>
2021-06-21 17:40:11 +0300

    Add the required imports to the controller

--- core-customize/hybris/bin/custom/sagocc/src/org/training/controllers/SagoccController.java
+++ core-customize/hybris/bin/custom/sagocc/src/org/training/controllers/SagoccController.java
@@ -3,6 +3,12 @@
  */
 package org.training.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
 @Controller("testController")
 @RequestMapping(value = "/{baseSiteId}/test")
 public class SagoccController 
