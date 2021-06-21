8e9195f513985901aeac50e04785bf5ece76c0ab
gepe <georgi.petrov@softwareag.com>
2021-06-21 16:11:37 +0300

    Add a training extension

---
+++ core-customize/hybris/bin/custom/training/src/org/training/constants/TrainingConstants.java
@@ -0,0 +1,14 @@
+package org.training.constants;
+
+@SuppressWarnings({"deprecation","squid:CallToDeprecatedMethod"})
+public class TrainingConstants extends GeneratedTrainingConstants
+{
+	public static final String EXTENSIONNAME = "training";
+	
+	private TrainingConstants()
+	{
+		//empty
+	}
+	
+	
+}
