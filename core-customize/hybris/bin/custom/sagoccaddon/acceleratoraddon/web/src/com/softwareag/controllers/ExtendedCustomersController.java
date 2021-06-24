468f4258b87f66d6265346f1a8e6f3e528f7e0ee
gepe <georgi.petrov@softwareag.com>
2021-06-24 11:37:40 +0300

    Try to extend the data model

---
+++ core-customize/hybris/bin/custom/sagoccaddon/acceleratoraddon/web/src/com/softwareag/controllers/ExtendedCustomersController.java
@@ -0,0 +1,40 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import de.hybris.platform.commerceservices.customer.DuplicateUidException;
+import de.hybris.platform.commercefacades.user.data.CustomerData;
+import de.hybris.platform.commercefacades.customer.CustomerFacade;
+
+/**
+ * Controller which extends Customer Resources
+ */
+@Controller("sampleExtendedCustomerController")
+@RequestMapping(value = "/{baseSiteId}/customers")
+public class ExtendedCustomersController 
+{
+
+	@Secured("ROLE_CUSTOMERGROUP")
+	@RequestMapping(value = "/current/nickname", method = RequestMethod.GET)
+	@ResponseBody		
+	public String getCustomerNickname()
+	{
+		final String name = getCustomerFacade().getCurrentCustomer().getNickname();
+		return name;
+	}
+
+	@Secured("ROLE_CUSTOMERGROUP")
+	@RequestMapping(value = "/current/nickname", method = RequestMethod.PUT)
+	@ResponseBody
+	public CustomerData setCustomerNickname(@RequestParam final String nickname) throws DuplicateUidException
+	{
+		CustomerFacade customerFacade = getCustomerFacade();
+		final CustomerData customer = customerFacade.getCurrentCustomer();
+		customer.setNickname(nickname);
+		customerFacade.updateFullProfile(customer);
+		return customerFacade.getCurrentCustomer();
+	}
+
+}
