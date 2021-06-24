4ad20a2138b0c57ba57a7cedba022f53d733685b
gepe <georgi.petrov@softwareag.com>
2021-06-24 14:15:07 +0300

    Add the new controller to the proper package

--- core-customize/hybris/bin/custom/sagoccaddon/acceleratoraddon/web/src/com/softwareag/controllers/ExtendedCustomersController.java
+++ core-customize/hybris/bin/custom/sagoccaddon/acceleratoraddon/web/src/com/softwareag/controllers/ExtendedCustomersController.java
@@ -1,3 +1,5 @@
+package com.softwareag.controllers;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
